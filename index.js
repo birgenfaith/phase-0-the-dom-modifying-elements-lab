46672bd4b734bd58e08bedf0f69326643f5626dc
Faith Chepkoech Birgen <faybirgen815@gmail.com>
2024-12-06 18:26:29 +0300

    add solutions on index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.textContent= "GAKENI is the champion";
